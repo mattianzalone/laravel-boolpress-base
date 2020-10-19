393ba8272b8d06445352f968ab127c62f27cc29d
mattia <mattia24.studio@gmail.com>
2020-10-19 13:58:01 +0200

    added

---
+++ app/Avatar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'telefono','avatar','user_id'
+    ];
+    public function user()
+    {
+    return $this->belongsTo('App\User');
+    }
+}
